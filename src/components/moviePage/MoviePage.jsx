a8c6676d6c8e81bba39c2a299cb02650cf52ed02
MohammedIzhar77 <izharmohammed21@gmail.com>
2024-04-06 01:25:08 +0530

    Added autocomplete feature

--- src/components/moviePage/MoviePage.jsx
+++ src/components/moviePage/MoviePage.jsx
@@ -3,42 +3,44 @@ import Navbar from "../navbar/Navbar";
 import MovieCard from "../movieCard/MovieCard";
 import axios from "axios";
 import searchMovie from "../../apis/omdb";
-import './MoviePage.css'
+import "./MoviePage.css";
 
 function MoviePage() {
   const [movieList, setMovieList] = useState([]);
 
   async function getMovies(...name) {
     console.log(name);
- const moviesarr =  name.map((name=> searchMovie(name))); 
+    const moviesarr = name.map((name) => searchMovie(name));
     console.log(moviesarr);
-  const response1= await axios.all(moviesarr.map((arr)=> axios.get(arr))).then((data)=>data)
-  console.log('here',response1);
-  const movies =  response1.map(res=>res.data.Search);
+    const response1 = await axios
+      .all(moviesarr.map((arr) => axios.get(arr)))
+      .then((data) => data);
+    console.log("here", response1);
+    const movies = response1.map((res) => res.data.Search);
     setMovieList([].concat(...movies));
-
-/*     const response = await axios(searchMovie(name.map(name=> name )));
+    /*     const response = await axios(searchMovie(name.map(name=> name )));
     console.log(response.data.Search);
     setMovieList(response.data.Search); */
   }
 
   useEffect(() => {
-    getMovies('avengers','spider man','harry potter');
+    getMovies("avengers", "spider man", "harry potter");
   }, []);
 
   return (
     <>
       <Navbar />
+      
       <div className="movie-up-wrapper">
-      {movieList.map((list) => (
-        <MovieCard
-          key={list.imdbID}
-          Poster={list.Poster}
-          Title={list.Title}
-          Year={list.Year}
-          Type={list.Type}
-        />
-      ))}
+        {movieList.map((list) => (
+          <MovieCard
+            key={list.imdbID}
+            Poster={list.Poster}
+            Title={list.Title}
+            Year={list.Year}
+            Type={list.Type}
+          />
+        ))}
       </div>
     </>
   );
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,42 @@
-import React from 'react'
-import './Navbar.css'
+import React, { useState } from "react";
+import "./Navbar.css";
 function Navbar() {
+  const [isShown, setIsShown] = useState(false);
   return (
-    <div className='navbar-wrapper'>
-    <div className='navbar-heading'>Movix</div>
-    <input className='navbar-input' type="text" placeholder='search here ...' />
-    <img className='theme-icon' src="https://cdn.hugeicons.com/icons/moon-02-stroke-rounded.svg" alt="moon-02" width="24" height="24" />
+    <div className="navbar-wrapper">
+      <div className="navbar-heading">Movix</div>
+      <input
+        className="navbar-input"
+        onFocus={() => {
+          console.log("on focus");
+          setIsShown(true);
+        }}
+        onBlur={() => {
+          console.log("on blur");
+          setIsShown(false);
+        }}
+        type="text"
+        placeholder="search here ..."
+      />
+
+      <img
+        className="theme-icon"
+        src="https://cdn.hugeicons.com/icons/moon-02-stroke-rounded.svg"
+        alt="moon-02"
+        width="24"
+        height="24"
+      />
+
+      {
+        isShown &&
+        <ul className="search-bar-autocomplete">
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+        </ul>
+      }
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
