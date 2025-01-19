64288a28d56e8ae1b6c7fad4bea976956838b565
MohammedIzhar77 <izharmohammed21@gmail.com>
2025-01-19 19:32:51 +0530

    Added custom mouse

--- src/components/Cursor.jsx
+++ src/components/Cursor.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import gsap from "gsap";
+import React, { useEffect } from "react";
 
 function Cursor() {
+  useEffect(() => {
+    const handeMouseMove = (event) => {
+      const { clientX, clientY } = event;
+      console.log(`clientX ${clientX , clientY}`);
+      
+      gsap.to("#cursor", {
+        x: clientX - 20/2,
+        y: clientY - 20/2,
+      });
+    };
+    window.addEventListener("mousemove", handeMouseMove);
+    return () => {
+      window.removeEventListener("mousemove", handeMouseMove);
+    };
+  }, []);
   return (
-    <div className='w-[20px] h-[20px] bg-white rounded-full absolute top-0 left-0'></div>
-  )
+    <div id="cursor" className="w-[20px] h-[20px] bg-white rounded-full absolute top-0 left-0"></div>
+  );
 }
 
-export default Cursor
+export default Cursor;
